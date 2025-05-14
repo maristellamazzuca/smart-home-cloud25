7f98334dd5bc50634e1518d511d02217c7bd16ab
maristellamazzuca <283362@studenti.unimore.it>
2025-05-14 11:40:09 +0200

    djdj

--- client/client.py
+++ client/client.py
@@ -6,19 +6,26 @@ from datetime import datetime, timezone
 SERVER_URL = 'https://smart-home-server-174184628218.europe-west1.run.app/receive_data'  # Sostituisci con il tuo vero URL
 
 def send_data(row):
-    unix_time = int(row['time'])  # converti stringa in intero
+    from datetime import datetime, timezone
+    unix_time = int(row['time'])
     timestamp = datetime.fromtimestamp(unix_time, tz=timezone.utc).isoformat()
 
     payload = {
         'timestamp': timestamp,
-        'value': row.get('use [kW]')  # Ottieni il valore dalla colonna 'use [kW]' in modo sicuro
+        'value': row['use [kW]']
     }
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
     print("Payload da inviare:", payload)
     try:
-        response = requests.post(SERVER_URL, json=payload)
+        response = requests.post(SERVER_URL, json=payload, headers=headers)
         print(f"Sent: {payload} → Status: {response.status_code}")
     except Exception as e:
         print(f"Error: {e}")
+
 
 def main():
     with open('client/HomeC.csv', newline='') as file:
